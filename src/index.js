4ad80ee780a71e7dc770542bcc73ff4abf5b0d6f
razzaksr <razzaksr@gmail.com>
2024-10-22 15:45:32 +0530

    22Oct2024

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Welcome to react development</h1>
+    <button className='col-lg-2 col-md-6 col-12 btn btn-outline-primary'>alpha</button>
+    <button className='btn btn-outline-danger'>beta</button>
+    <button className='btn btn-outline-success'>cosmo</button>
+    <button className='btn btn-outline-dark'>delta</button>
   </React.StrictMode>
 );
 
